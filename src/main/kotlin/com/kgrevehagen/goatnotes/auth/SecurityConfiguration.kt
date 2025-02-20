946d56707c5567fdb4d351fa9f3a05fbb3494804
Kristian Greve Hagen <kristian.greve.hagen@gmail.com>
2025-02-20 11:39:33 +0100

    Add SecurityConfiguration for added security
    In short, this checks so that we need to be authenticated for all requests, as well as dealing with giving us the jwt for logged in users.

---
+++ src/main/kotlin/com/kgrevehagen/goatnotes/auth/SecurityConfiguration.kt
@@ -0,0 +1,31 @@
+package com.kgrevehagen.goatnotes.auth
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+internal class SecurityConfiguration {
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf { it.disable() }
+            .authorizeHttpRequests(
+                {
+                    it.requestMatchers("/").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                }
+            )
+            .oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
+            .build()
+    }
+}
