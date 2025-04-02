c2317d01a3911395189dd83ecc2c2eb2c577f2bf
Kristian Greve Hagen <kristian.greve.hagen@gmail.com>
2025-04-02 17:13:55 +0200

    Check that notesController is initialized
    Just for showcasing that we can do it like this, it will most likely not be needed when we start having integratino tests that uses notesController.

--- src/test/kotlin/com/kgrevehagen/goatnotes/AppTests.kt
+++ src/test/kotlin/com/kgrevehagen/goatnotes/AppTests.kt
@@ -1,13 +1,18 @@
 package com.kgrevehagen.goatnotes
 
+import assertk.assertThat
+import assertk.assertions.isNotNull
+import com.kgrevehagen.goatnotes.notes.NotesController
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest(properties = ["spring.profiles.active=test"])
-class AppTests {
+internal class AppTests @Autowired constructor(private val notesController: NotesController) {
 
 	@Test
 	fun contextLoads() {
+		assertThat(notesController).isNotNull()
 	}
 
 }
