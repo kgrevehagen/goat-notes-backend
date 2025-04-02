f9e06a69d8c6a5d2db84916583a0ed98030a171f
Kristian Greve Hagen <kristian.greve.hagen@gmail.com>
2025-04-02 17:25:40 +0200

    Configure stateless session management in SecurityConfiguration
    Not strictly necessary, but aligns with best practices. Prevents Spring Security from creating unnecessary sessions.

--- src/main/kotlin/com/kgrevehagen/goatnotes/auth/SecurityConfiguration.kt
+++ src/main/kotlin/com/kgrevehagen/goatnotes/auth/SecurityConfiguration.kt
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -25,6 +26,7 @@ internal class SecurityConfiguration {
                         .authenticated()
                 }
             )
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
             .build()
     }
